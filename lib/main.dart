fa78b38af2501fb99457a0d3598dbf1ede1f8f68
duke <webcreation33@yahoo.fr>
2022-02-28 17:02:37 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_shop/providers/cart.dart';
+import 'package:flutter_app_shop/providers/orders.dart';
+import 'package:flutter_app_shop/providers/products.dart';
+import 'package:flutter_app_shop/screens/cart_screen.dart';
+import 'package:flutter_app_shop/screens/edit_product_screen.dart';
+import 'package:flutter_app_shop/screens/orders_screen.dart';
+import 'package:flutter_app_shop/screens/product_detail_screen.dart';
+import 'package:flutter_app_shop/screens/products_overview_screen.dart';
+import 'package:flutter_app_shop/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
+        ),
+        /*
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
+        ),
+        */
+      ],
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
+            OrdersScreen.routeName: (ctx) => OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          }),
+    );
+  }
+}
